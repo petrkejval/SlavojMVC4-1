9ea01276c3c5272dc0ec1381253c7db856a7462a
Petr Kejval <petr.kejval@gmail.com>
2014-12-03 14:38:54 +0100

    Uloženy změny před programováním Výsledků

--- SlavojMVC4-1/Controllers/MenuController.cs
+++ SlavojMVC4-1/Controllers/MenuController.cs
@@ -36,6 +36,44 @@ namespace SlavojMVC4_1.Controllers
             var id = db.Kluby.Max(m => m.KlubId);
             var model = db.Kluby.Find(id);
 
+            ViewBag.Prezident =
+            db.Cleni.Where(w =>
+                                (w.CleniInRoles.Where(b => b.ClenRoleId == 1).Count() > 0)  //musí být prezident
+                          )
+                          .FirstOrDefault()
+                          as SlavojMVC4_1.Models.Clen;
+
+            ViewBag.VicePrezident =
+            db.Cleni.Where(w =>
+                                (w.CleniInRoles.Where(b => b.ClenRoleId == 2).Count() > 0)  //musí být viceprezident
+                          )
+                          .FirstOrDefault()
+                          as SlavojMVC4_1.Models.Clen;
+
+            ViewBag.Pokladnik =
+            db.Cleni.Where(w =>
+                                (w.CleniInRoles.Where(b => b.ClenRoleId == 3).Count() > 0)  //musí být pokladník
+                          )
+                          .FirstOrDefault()
+                          as SlavojMVC4_1.Models.Clen;
+
+            ViewBag.OrganizacniPracovnikKlubu =
+                db.Cleni.Where(w =>
+                                    (w.CleniInRoles.Where(b => b.ClenRoleId == 5).Count() > 0)  //musí být organizační pracovník
+                              )
+                              .FirstOrDefault()
+                              as SlavojMVC4_1.Models.Clen;
+
+
+            ViewBag.CleniVyboruPouze =
+                db.Cleni.Where(w =>
+                                    (w.CleniInRoles.Where(b => b.ClenRoleId == 4).Count() > 0)  //musí být člem výboru
+                                 && (w.CleniInRoles.Where(c => c.ClenRoleId == 1).Count() == 0) //nesmí být prezident
+                                 && (w.CleniInRoles.Where(c => c.ClenRoleId == 2).Count() == 0) //nesmí být viceprezident
+                                 && (w.CleniInRoles.Where(c => c.ClenRoleId == 3).Count() == 0) //nesmí být pokladník
+                              )
+                              .OrderBy(o => o.Prijmeni).ThenBy(o => o.Jmeno)
+                              as IEnumerable<SlavojMVC4_1.Models.Clen>;
             return View(model);
         }
     }
